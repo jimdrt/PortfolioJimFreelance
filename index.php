1027c8724ef28030105008381b31b76951d57bc6
Duarte Jimmy <jimmyduartepro@gmail.com>
2022-11-22 09:22:49 +0100

    first add

---
+++ index.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+
+<head>
+  <title>JimFreelance</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="assets/css/main.css" />
+  <noscript>
+    <link rel="stylesheet" href="assets/css/noscript.css" />
+  </noscript>
+</head>
+
+<body class="is-preload">
+  <!-- Sidebar -->
+  <section id="sidebar">
+    <div class="inner">
+      <nav>
+        <ul>
+          <li><a href="#intro">Accueil</a></li>
+          <li><a href="#one">À propos</a></li>
+          <li><a href="#two">Prestations</a></li>
+          <li><a href="#three">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </section>
+
+  <!-- Wrapper -->
+  <div id="wrapper">
+    <!-- Intro -->
+    <section id="intro" class="wrapper style1 fullscreen fade-up">
+      <div class="inner">
+        <h1>JimFreelance</h1>
+        <p>
+          Développez votre entreprise et démarquez-vous de vos concurents
+          grâce à nos sites Internet.
+        </p>
+        <ul class="actions">
+          <!-- <li><a href="#one" class="button scrolly">En savoir plus</a></li> -->
+          <li><a href="#three" class="button scrolly">En savoir plus</a></li>
+        </ul>
+      </div>
+    </section>
+
+    <!-- One -->
+    <section id="one" class="wrapper style2 spotlights">
+      <section>
+        <a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+        <div class="content">
+          <div class="inner">
+            <h2>Création de sites Wordpress</h2>
+            <p>
+              Création de sites Internet sous le CMS Wordpress, avec pour
+              avantage de vous conduire vers l'autonomie. Pas besoin
+              d'appeller un développeur pour un simple changement d'image.
+            </p>
+            <ul class="actions">
+              <li>
+                <!-- <a href="generic.html" class="button">En savoir plus</a> -->
+                <a href="#three" class="button">En savoir plus</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section>
+        <a href="#" class="image"><img src="images/pic02.png" alt="" data-position="top center" /></a>
+        <div class="content">
+          <div class="inner">
+            <h2>Référencement SEO</h2>
+            <p>
+              Le SEO pour Search Engine Optimisation est un ensemble de
+              techniques visant à optimiser la visibilité d'un site web dans
+              les pages de résultats des moteurs de recherche afin de vous
+              permettre de vous demarquer de vos concurrents.
+            </p>
+            <ul class="actions">
+              <li>
+                <!-- <a href="generic.html" class="button">En savoir plus</a> -->
+                <a href="#three" class="button">En savoir plus</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <section>
+        <a href="#" class="image"><img src="images/pic03.png" alt="" data-position="25% 25%" /></a>
+        <div class="content">
+          <div class="inner">
+            <h2>Ultricies aliquam</h2>
+            <p>
+              Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+              turpis mauris, eu ultricies erat malesuada quis. Aliquam
+              dapibus.
+            </p>
+            <ul class="actions">
+              <!-- <li><a href="generic.html" class="button">Learn more</a></li> -->
+              <li><a href="#three" class="button">En savoir plus</a></li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </section>
+
+    <!-- Two -->
+    <section id="two" class="wrapper style3 fade-up">
+      <div class="inner">
+        <h2>Nos prestations</h2>
+        <p>
+          Chez JimFreelance, nous avons à coeur de soutenir les entreprises
+          avec qui nous faisons équipe! Voici nos prestations, permettant de
+          vous aider au mieux dans la création de votre projet.
+        </p>
+        <div class="features">
+          <section>
+            <span class="icon solid major fa-code"></span>
+            <h3>Création de sites Internet</h3>
+            <p>
+              Nous utilisons Wordpress, un CMS très puissant vous permettant,
+              une fois livré, de vous rendre autonome sur votre projet
+              (changement d'image, ajout de plugin, modification de contenu et
+              bien d'autres options)
+            </p>
+          </section>
+          <section>
+            <span class="icon solid major fa-lock"></span>
+            <h3>Référencement SEO</h3>
+            <p>
+              Le SEO pour Search Engine Optimisation est un ensemble de
+              techniques visant à optimiser la visibilité d'un site web dans
+              les pages de résultats des moteurs de recherche afin de vous
+              permettre de vous demarquer de vos concurrents.
+            </p>
+          </section>
+          <section>
+            <span class="icon solid major fa-cog"></span>
+            <h3>Responsive Design</h3>
+            <p>
+              Un site web responsive est un site web dont la conception vise,
+              grâce à différents principes et techniques, à offrir une
+              consultation confortable sur des écrans de tailles très
+              différentes. Afin que l'utilisateur puisse naviguer au sein de
+              votre site web avec son téléphone ou sa tablette.
+            </p>
+          </section>
+          <section>
+            <span class="icon solid major fa-desktop"></span>
+            <h3>Gestion des réseaux sociaux</h3>
+            <p>
+              Vous ciblez une clientèle particulière, nous nous occupons de la
+              gestion de vos réseaux sociaux comme TikTok, Instagram ou encore
+              Twitter. Avec une garantie d'augmenter considérablement votre
+              nombre de followers. Satisfait ou remboursé!
+            </p>
+          </section>
+          <section>
+            <span class="icon solid major fa-link"></span>
+            <h3>Intégration</h3>
+            <p>
+              Afin d'optimiser la personnalisation de votre site Internet,
+              nous proposons d'intégrer du code dans votre site Internet afin
+              d'avoir une image propre à votre entreprise. N'oublions pas que
+              wordpress représente 70% de la toile.
+            </p>
+          </section>
+          <section>
+            <span class="icon major fa-gem"></span>
+            <h3>Un travail de qualité</h3>
+            <p>
+              Nous avons à coeur de fournir un travail de qualité afin de vous
+              permettre de faire évoluer votre entreprise et les PME du
+              Sud-Ouest. Vous faire grandir, nous fais grandir également.
+            </p>
+          </section>
+        </div>
+        <!-- <ul class="actions">
+            <li><a href="generic.html" class="button">Learn more</a></li>
+          </ul> -->
+      </div>
+    </section>
+
+    <!-- Three -->
+    <section id="three" class="wrapper style1 fade-up">
+      <div class="inner">
+        <h2>Besoin d'un devis?</h2>
+        <p>
+          Parlez-nous de votre projet, ce que vous souhaitez et en quoi nous
+          pouvons vous aider. Nous vous recontacterons dans les plus brefs
+          délais.
+        </p>
+        <div class="split style1">
+          <section>
+            <form method="POST">
+              <div class="fields">
+                <div class="field half">
+                  <label for="name">Nom</label>
+                  <input id="nameForm" type="text" name="name" id="name" />
+                </div>
+                <div class="field half">
+                  <label for="email">Email</label>
+                  <input id="emailForm" type="email" name="email" id="email" required />
+                </div>
+                <div class="field">
+                  <label for="message">Message</label>
+                  <textarea name="message" id="message" rows="5"></textarea>
+                </div>
+              </div>
+              <ul class="actions">
+                <li>
+                  <a href="#" name="submit" type="submit" id="submit" class="button submit">Envoyer</a>
+                </li>
+              </ul>
+            </form>
+
+            <?php
+            if (isset($_POST["submit"])) {
+              $retour = mail("jimfreelancepro@gmail.com", $_POST["name"], $_POST["message"], "Reply-to:" . $_POST["email"]);
+              if ($retour) {
+                echo "<p>L'email à bien été envoyé</p>";
+              }
+            }
+            ?>
+          </section>
+          <section>
+            <ul class="contact">
+              <li>
+                <h3>Adresse</h3>
+                <span>38 rue Carnot, 64000 PAU</span>
+              </li>
+              <li>
+                <h3>Email</h3>
+                <a href="mailto:jimfreelancepro@gmail.com">jimfreelancepro@gmail.com</a>
+              </li>
+              <li>
+                <h3>Téléphone</h3>
+                <span>06 35 47 04 56</span>
+              </li>
+              <!-- <li>
+                  <h3>Social</h3>
+                  <ul class="icons">
+                    <li>
+                      <a href="#" class="icon brands fa-twitter"
+                        ><span class="label">Twitter</span></a
+                      >
+                    </li>
+                    <li>
+                      <a href="#" class="icon brands fa-facebook-f"
+                        ><span class="label">Facebook</span></a
+                      >
+                    </li>
+                    <li>
+                      <a href="#" class="icon brands fa-github"
+                        ><span class="label">GitHub</span></a
+                      >
+                    </li>
+                    <li>
+                      <a href="#" class="icon brands fa-instagram"
+                        ><span class="label">Instagram</span></a
+                      >
+                    </li>
+                    <li>
+                      <a href="#" class="icon brands fa-linkedin-in"
+                        ><span class="label">LinkedIn</span></a
+                      >
+                    </li>
+                  </ul>
+                </li> -->
+            </ul>
+          </section>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <!-- Footer -->
+  <footer id="footer" class="wrapper style1-alt">
+    <div class="inner">
+      <ul class="menu">
+        <li>&copy; JimFreelancePro</li>
+      </ul>
+    </div>
+  </footer>
+
+  <!-- Scripts -->
+  <script>
+    const name = document.getElementById("nameForm");
+    const email = document.getElementById("emailForm");
+    const message = document.getElementById("message");
+    const submit = document.getElementsByClassName("form-contact")[0];
+
+    submit.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log("clicked");
+    });
+  </script>
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/jquery.scrollex.min.js"></script>
+  <script src="assets/js/jquery.scrolly.min.js"></script>
+  <script src="assets/js/browser.min.js"></script>
+  <script src="assets/js/breakpoints.min.js"></script>
+  <script src="assets/js/util.js"></script>
+  <script src="assets/js/main.js"></script>
+  <script src="https://smtpjs.com/v3/smtp.js"></script>
+</body>
+
+</html>
